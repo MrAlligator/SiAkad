278fc449f115e6fab6dc54a0317091fd1eaf6007
ZL XXIV <55338602+zl-xxiv@users.noreply.github.com>
2022-04-17 19:28:52 +0700

    .

--- application/views/backend/data/jadwal/index.php
+++ application/views/backend/data/jadwal/index.php
@@ -29,16 +29,27 @@
                             <th>Mata Pelajaran</th>
                             <th>Kelas</th>
                             <th>Jurusan</th>
+                            <th>Hari</th>
                             <th>Waktu</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>Matematika</th>
-                            <th>'] ?></th>
-                            <th>'] ?></th>
-                            <th>'] ?></th>
-                        </tr>
+                        <?php foreach ($jadwal as $jadwal) {
+                            $id_kelas = $jadwal['id_kelas'];
+                            $kelas = $this->db->where(['id_kelas' => $id_kelas])->get('tb_kelas')->row_array();
+                            $id_jurusan = $jadwal['id_jurusan'];
+                            $jurusan = $this->db->where(['id_jurusan' => $id_jurusan])->get('tb_jurusan')->row_array();
+                            $kode_mapel = $jadwal['kode_mapel'];
+                            $mapel = $this->db->where(['kode_mapel' => $kode_mapel])->get('tb_mapel')->row_array();
+                        ?>
+                            <tr>
+                                <th><?= $mapel['nama_mapel'] ?></th>
+                                <th><?= $kelas['kelas'] ?></th>
+                                <th><?= $jurusan['jurusan'] ?></th>
+                                <th><?= $jadwal['hari'] ?></th>
+                                <th><?= $jadwal['jam'] ?></th>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
