8016d478431ee2f773500fae09bb89a4e8b67f09
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2022-05-30 08:37:05 +0700

    Update index.php

--- application/views/backend/home/index.php
+++ application/views/backend/home/index.php
@@ -46,7 +46,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Karyawan
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Karyawan (Branch Secondary)
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
@@ -69,7 +69,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Jumlah Pendaftar (Branch Kembangkan)</div>
+                                Jumlah Pendaftar</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                         </div>
                         <div class="col-auto">
