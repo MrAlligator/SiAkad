5cb5e5980d5430dd41e5d87cffa00ca8fd80819c
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2022-05-30 08:32:19 +0700

    Update index.php

--- application/views/backend/home/index.php
+++ application/views/backend/home/index.php
@@ -69,7 +69,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Jumlah Pendaftar</div>
+                                Jumlah Pendaftar (Branch Kembangkan)</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                         </div>
                         <div class="col-auto">
